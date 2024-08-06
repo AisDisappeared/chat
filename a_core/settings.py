19602e35922429d131eebbd29ad8fe74695e1fbd
Ali <Alihunter4640@gmail.com>
2024-08-06 21:41:04 +0330

    django admin panel imrpoved

--- a_core/settings.py
+++ a_core/settings.py
@@ -33,6 +33,7 @@ CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
